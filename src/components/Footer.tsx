d4c9285e9a563007ce7c1f9ea1d7cd97ed28de34
Franckie <franckie.dev@gmail.com>
2025-10-10 10:41:33 +0300

    feat(layout): add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="shadow-xl text-center mt-auto bg-base-200 opacity-60">
+      <div className="container mx-auto px-4 py-6">Powerd by TMDB API</div>
+    </footer>
+  )
+}
+
+export default Footer
