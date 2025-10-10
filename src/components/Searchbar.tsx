6b3fd714156c4e4e6a8aa92c23c20128d6ce5ba4
Franckie <franckie.dev@gmail.com>
2025-10-10 10:45:05 +0300

    feat(search): add Searchbar component

---
+++ src/components/Searchbar.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react"
+import { useDebounce } from "../hooks/useDebounce.ts"
+
+type SearchbarProps = {
+  onSearch: (query: string) => void
+}
+
+const Searchbar = ({ onSearch }: SearchbarProps) => {
+  const [searchTerm, setSearchTerm] = useState("")
+  const debouncedValue = useDebounce(searchTerm, 500)
+
+  useEffect(() => {
+    onSearch(debouncedValue)
+  }, [debouncedValue, onSearch])
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    onSearch(searchTerm)
+  }
+
+  return (
+    <form className="container mx-auto px-4" onSubmit={handleSubmit}>
+      <label className="input w-full">
+        <svg
+          className="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </g>
+        </svg>
+        <input
+          type="search"
+          className="grow"
+          placeholder="Search a movie..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </label>
+    </form>
+  )
+}
+
+export default Searchbar
