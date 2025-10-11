9665349403011d0af6877bac6a7df2d635ce4778
Franckie <franckie.dev@gmail.com>
2025-10-11 07:04:55 +0300

    refactor(hooks): improve useSearchMovies with better abort and error handling

--- src/hooks/useSearchMovies.ts
+++ src/hooks/useSearchMovies.ts
@@ -1,46 +1,49 @@
-import { useCallback, useRef, useState } from "react"
-import { searchMovie } from "../services/api.ts"
+import { useCallback, useState } from "react"
 import type { Movie, StatusType } from "../types/types.ts"
+import { searchMovie } from "../services/searchMovie.ts"
 
 export const useSearchMovies = () => {
   const [movies, setMovies] = useState<Movie[]>([])
   const [status, setStatus] = useState<StatusType>("idle")
   const [error, setError] = useState<string | null>(null)
-  const abortControllerRef = useRef<AbortController | null>(null)
 
-  const search = useCallback(async (searchTerm: string) => {
-    if (!searchTerm.trim()) {
-      reset()
+  const search = useCallback(async (query: string) => {
+    if (!query.trim()) {
+      setStatus("idle")
+      setMovies([])
+      setError(null)
       return
     }
 
-    abortControllerRef.current?.abort()
-    abortControllerRef.current = new AbortController()
-
-    setStatus("loading")
-    setError(null)
+    const controller = new AbortController()
 
     try {
-      const data = await searchMovie(searchTerm)
+      setStatus("loading")
+      setError(null)
+
+      const data = await searchMovie(query, controller.signal)
+
       setMovies(data.results)
       setStatus("success")
-    } catch (error) {
-      if (error instanceof Error && error.name === "AbortError") {
+    } catch (err) {
+      if (err instanceof Error && err.name === "CanceledError") {
         return
       }
-      const errorMessage =
-        error instanceof Error ? error.message : "Une erreur est survenue"
-      setError(errorMessage)
+      const errMessage =
+        err instanceof Error ? err.message : "An unexpected occured"
+
+      setError(errMessage)
       setStatus("error")
       setMovies([])
     }
+
+    return () => controller.abort
   }, [])
 
   const reset = useCallback(() => {
-    abortControllerRef.current?.abort()
     setMovies([])
-    setError(null)
     setStatus("idle")
+    setError(null)
   }, [])
 
   return {
