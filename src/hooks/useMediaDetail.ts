5184d0fedd7466535bca38c990533cd754505ad7
Franckie <franckie.dev@gmail.com>
2025-10-16 14:00:29 +0300

    refactor: rename getMovieDetail to getMediaDetail and update imports

--- src/hooks/useMediaDetail.ts
+++ src/hooks/useMediaDetail.ts
@@ -1,11 +1,11 @@
 import { useQuery } from "@tanstack/react-query"
-import { getMovieDetail } from "../services/getMovieDetail.ts"
+import { getMediaDetail } from "../services/getMediaDetail.ts"
 import type { MediaType } from "../types/types.ts"
 
 export const useMediaDetail = (mediaType: MediaType, mediaItemId: string) => {
   return useQuery({
     queryKey: ["detail", mediaItemId, mediaType],
-    queryFn: () => getMovieDetail(mediaType, mediaItemId),
+    queryFn: () => getMediaDetail(mediaType, mediaItemId),
     enabled: !!mediaType && !!mediaItemId,
   })
 }
