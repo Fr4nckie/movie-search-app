4e45916b6618ffd73fe507d87d05e5710db73798
Franckie <franckie.dev@gmail.com>
2025-10-20 13:36:51 +0300

    feat(theme): add ToggleThemeButton and use it in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom"
+import ToggleThemeButton from "./ToggleThemeButton.tsx"
 
 const Header = () => {
   return (
     <header className="py-4 shadow mb-4">
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-4 flex items-center justify-between">
         <Link to="/">
           <h1 className="text-2xl font-bold capitalize">movie search app</h1>
         </Link>
+        <ToggleThemeButton />
       </div>
     </header>
   )
